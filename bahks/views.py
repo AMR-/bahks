20d236a3084579744b517f18f7d54a814e099d22
Austin Robinson <austin.rbsn@gmail.com>
2013-06-01 18:58:11 -0400

    Adds signup form.

--- bahks/views.py
+++ bahks/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login, logout
+import forms
 
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    form = forms.UserForm()
+    return render(request, 'signup.html', {'form': form})
