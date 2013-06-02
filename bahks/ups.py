9bded52f7b23bf39adb0f8bf5627babe66ebf92c
Matt Garbis <mgarbis@gmail.com>
2013-06-02 10:41:37 -0400

    fixed bugs in ups module and added PIL support

--- bahks/ups.py
+++ bahks/ups.py
@@ -5,21 +5,20 @@ https://pypi.python.org/pypi/ClassicUPS
 
 from ClassicUPS.ups import UPSConnection
 import credentials
-<<<<<<< HEAD
+
 from PIL import Image
-=======
->>>>>>> b14899ac9ee646dfb2514558e6ea16b47b147072
+
 
 ups = UPSConnection(license_number=credentials.ups_access_key, 
     user_id='bahksme', password=credentials.ups_password, 
     shipper_number=credentials.ups_account_number)
 
 
-<<<<<<< HEAD
+
 # returns a shipment object
 # service options are: '1dayair','2dayair','ground',worldwide_expedited'
 
-=======
+
 # helper method for set_up_shipment
 def create_shipment(from_addr, to_addr, dimensions, weight, shipping_service):
     shipment = ups.create_shipment(from_addr, to_addr, dimensions, weight, 
@@ -28,7 +27,7 @@ def create_shipment(from_addr, to_addr, dimensions, weight, shipping_service):
 
 # returns a shipment object
 # service options are: '1dayair','2dayair','ground',worldwide_expedited'
->>>>>>> b14899ac9ee646dfb2514558e6ea16b47b147072
+
 def set_up_shipment(from_name, from_street_address, from_city, 
     from_state, from_country, from_zip, from_phone, to_name, 
     to_street_address, to_city, to_state, to_country, 
@@ -67,7 +66,7 @@ def create_shipment(from_addr, to_addr, dimensions, weight, shipping_service):
 
 # creates a shipping label gif (saves it in root?)
 def save_label(shipment):
-    return shipment.save_label(open('./tmp/label.gif', 'wb'))
+    return shipment.save_label(open('label.gif', 'wb'))
 
 def get_image_object(shipment):
     raw_epl = shipment.accept_result.dict_response['ShipmentAcceptResponse']
